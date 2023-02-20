fd340fd476de86a97a084c11bada5536e29f52a3
almosrof120 <mosrof35-645@diu.edu.bd>
2023-02-21 01:40:41 +0600

    5th commit

--- 21.dart
+++ 21.dart
@@ -1,6 +1,11 @@
 void main(List<String> args) {
+<<<<<<< HEAD
   print("i love to coding");
 }
 
 // stay well 
 // good to see you
+=======
+  print("I love coding");
+}
+>>>>>>> 90af774 (5th commit)
