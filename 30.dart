ec70ef27bd005e93ace70048311f8e300145c1bb
almosrof120 <mosrof35-645@diu.edu.bd>
2023-09-10 02:03:36 +0600

    1

--- 30.dart
+++ 30.dart
@@ -1,10 +1,14 @@
-void main(List<String> args) {
-  var age = 30;
-  if (age > 18) {
-    print("Adult");
-  } else if (age < 10) {
-    print("Child");
-  } else {
-    print("Invalid");
-  }
-}
+// void main(List<String> args) {
+//   var age = 30;
+//   if (age > 18) {
+//     print("Adult");
+//   } else if (age < 10) {
+//     print("Child");
+//   } else {
+//     print("Invalid");
+//   }
+// }
+
+
+
+
