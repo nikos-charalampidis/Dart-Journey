fe21ed5f04acd30c45ded9e43d0e9f3a416b764c
almosrof120 <mosrof35-645@diu.edu.bd>
2023-09-11 00:15:00 +0600

    hashmap for loop

---
+++ 34.dart
@@ -0,0 +1,15 @@
+import 'dart:collection';
+
+void main(List<String> args) {
+  var hashmap = new HashMap();
+  hashmap["Integer:"]= 100;
+  hashmap["Float:"]= 10.6;
+  hashmap["Bol:"]= "True";
+  hashmap["String:"]= "Shinul";
+
+  var myvalue;
+  for(myvalue in hashmap.keys){
+    print(hashmap);
+  }
+
+}
