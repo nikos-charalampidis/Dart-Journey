bdf7964e34d76109b6d416784d5fff5ea752d674
almosrof120 <mosrof35-645@diu.edu.bd>
2023-09-12 01:47:49 +0600

    object create in dart main method

---
+++ 41.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  
+  var obj = new Tmyclass();
+  obj.Addtwo();
+  obj.AdThree();
+}
+
+
+class Tmyclass{
+  int a= 100;
+  int b= 200;
+   var c ;
+
+  
+  void Addtwo(){
+    var name = "Shinul";
+    print(name);
+  }
+    void AdThree(){
+      print(c=a+b);
+    }
+  
+}
