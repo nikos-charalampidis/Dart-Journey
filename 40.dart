177e2d6394b0024a0c1334076fc7861a6c33c3cb
almosrof120 <mosrof35-645@diu.edu.bd>
2023-09-12 01:11:53 +0600

    class, local & global variables

---
+++ 40.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  var Myobject = Myclass();
+  Myobject.addTwo();
+}
+
+class Myclass{
+
+  var name = "Shinul";
+  var Age = 200;
+
+
+void addThe(){
+  var full_name = "Al Mosrof Mia";
+  print(full_name);
+}
+
+void addTwo(){
+  print(name);
+}
+}
+
