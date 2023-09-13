ed1fb63880b3da2936ebb2a654f3c9a140cc6a6a
almosrof120 <mosrof35-645@diu.edu.bd>
2023-09-14 00:56:15 +0600

    constructor method using in class for passing data

---
+++ 61.dart
@@ -0,0 +1,7 @@
+import '62.dart';
+
+void main(List<String> args) {
+  var obj = shinul("Al Mosrof Mia", 23);
+  print(obj.name);
+  print(obj.age);
+}
---
+++ 62.dart
@@ -0,0 +1,11 @@
+class shinul{
+var name;
+var age;
+
+
+  shinul(var a, var b){
+name = a;
+age = b;
+
+  }
+}
