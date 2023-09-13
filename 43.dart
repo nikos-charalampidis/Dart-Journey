14fa3549fc01e040bebfc39186fa9bb85ab2793f
almosrof120 <mosrof35-645@diu.edu.bd>
2023-09-13 21:19:58 +0600

    create external class

--- 43.dart
+++ 43.dart
@@ -1,15 +1,11 @@
 class Al{
 
 var name = "AL Mosrof Mia";
-
 void addTwo(int a, int b){
   print(a+b);
 }
 
-
 void New(){
   print(name);
 }
-
-
 }
--- 44.dart
+++ 44.dart
@@ -4,5 +4,6 @@ void main(List<String> args) {
   
   var obj = new Al();
   obj.New();
+  
   obj.addTwo(50, 100);
 }
